648385e635dd63ac3aff5269d98a785ba339e840
AjimTamboli-Web <ajimtamboli313@gmail.com>
2026-03-01 11:25:53 +0530

    using Clone Method

--- C_Deep_Copy_with_cloneMethod.java
+++ C_Deep_Copy_with_cloneMethod.java
@@ -5,27 +5,38 @@ import java.util.List;
 
 public class C_Deep_Copy_with_cloneMethod {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CloneNotSupportedException {
 
 /*
  * A deep copy creates a new object and also creates new copies of all referenced objects inside it.
  * 
  * The process of creating exactly duplicate independent copy including contain object,
- * is called deep cloning.
- * In deep cloning if the main object contain any primitive veriables then in the clone object 
- * duplicate copies will be created.
- * If the main object contain anhy reference variable then corresponding contain object alson will be 
- * created in the cloned copy.
+    is called deep cloning.
+ * In deep cloning if the main object contain any primitive variables then in the clone object 
+   duplicate copies will be created.
+ * If the main object contain any reference variable then corresponding contain object also will be 
+    created in the cloned copy.
  * By default object class clone method meant for shallow cloning, but we can implement deep cloning 
- * explicitly by overriding clone() method in our class.
+    explicitly by overriding clone() method in our class.
  * 	
  * By using clone object reference if we perform any change to the contain object then those changes
- * won't reflected to the main object.
+    won't reflected to the main object.
+ * 
  * Which cloning is Best??
  * if object contains only primitive variables then shallow cloning is the Best choice.
  * if object contains reference variables then deep cloning is the best choice.
  * 
- * 	
+ * 	but,Joshua Bloch strongly recommends:
+									❌ Avoid clone()
+ *  Now the important part 👇
+        If we don’t use clone(), then what should we use instead?		
+        
+ * 		✅ 1️⃣ Copy Constructor (Most Recommended)
+		This is the cleanest and safest way.
+ *      ✅ 2️⃣ Static Factory Method Copy
+		Sometimes better than constructor:  
+ *        							
+									
  */
 		List<String> li = List.of("John","Derek","Lucy");
 				Dept cm = new Dept("Computer",li);
@@ -57,20 +68,29 @@ class College implements Cloneable{
 		System.out.println("Constructor call....");
 	}
 	
-	public Object clone() {
-		Dept de2 = new Dept(de.name,de.li);
-		College col = new College(name,sal,de2);
-		return col;
+	public Object clone() throws CloneNotSupportedException {
+		// this process bypass the constructor
+		College co = (College)super.clone();
+		co.de = (Dept)de.clone();
+		
+		// this process call constructor
+//		Dept de2 = new Dept(de.name,de.li);
+//		College col = new College(name,sal,de2);
+		return co;
 	}
 	
 }
 
-class Dept{
+class Dept implements Cloneable{
 	String name;
 	List<?> li = new ArrayList<>();
 	Dept(String name, List<?> li){
 		this.name = name;
 		this.li = li;
+	}
+	
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
 	}
 	
 	
